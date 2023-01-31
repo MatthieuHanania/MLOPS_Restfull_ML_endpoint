bfb6540a213b1ab0c184273d698d0eb885e5342b
Matthieu Hanania <71183300+MatthieuHanania@users.noreply.github.com>
2023-01-31 19:39:35 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+
+
+app = Flask(__name__)
+
+model = keras.models.load_model('mnistModel')
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    image_data = request.get_json()
+    query = pd.DataFrame(image_data)
+    query = query/255
+    prediction = model.predict(query).tolist()
+    print(prediction)
+    return prediction
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
