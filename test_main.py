1904dd5ea6a99f26da9ae8c558cf77f75aee37c3
Matthieu Hanania <71183300+MatthieuHanania@users.noreply.github.com>
2023-02-03 20:42:58 +0100

    Create  test_main.py

---
+++ test_main.py
@@ -0,0 +1,34 @@
+import unittest
+import keras
+import pandas
+from app import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_classify(self):
+        response = self.app.get('/classify')
+        self.assertEqual(response.status_code, 200)
+        
+        #Testing if the model is good or not
+        model = keras.models.load_model('mnistModel.h5')
+        
+        df_test = pd.read_csv("fashion-mnist_test.csv")
+        X_test = df_test.loc[0][1:]/255
+        Y_test = df_test.loc[0].label
+        
+        prediction= model.predict(X_test).tolist()
+        self.assertEqual(response.json['name'], 'ben')
+        max = np.argmax(prediction)
+        self.assertEqual(max, 0)
+
+if __name__ == '__main__':
+    unittest.main()
