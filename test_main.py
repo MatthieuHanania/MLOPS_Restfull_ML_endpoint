05fae482661897dcb55c56c93c4ef99a05c1608f
Matthieu Hanania <71183300+MatthieuHanania@users.noreply.github.com>
2023-02-03 20:50:37 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -8,12 +8,7 @@ class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
-
-    def test_home(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-
-
+        
     def test_classify(self):
         response = self.app.get('/classify')
         self.assertEqual(response.status_code, 200)
