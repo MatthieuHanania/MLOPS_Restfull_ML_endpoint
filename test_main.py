1a7e11dc9c8cee0836dec5823d0fc6114397bbc9
Matthieu Hanania <71183300+MatthieuHanania@users.noreply.github.com>
2023-02-03 21:04:24 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -18,7 +18,9 @@ class FlaskTestCase(unittest.TestCase):
         X_test = df_test.loc[0][1:]/255
         Y_test = df_test.loc[0].label
         
-        prediction= model.predict([X_test]).tolist()
+        
+        
+        prediction= model.predict(pd.DataFrame([X_test])).tolist()
         self.assertEqual(response.json['name'], 'ben')
         max = np.argmax(prediction)
         self.assertEqual(max, 0)
